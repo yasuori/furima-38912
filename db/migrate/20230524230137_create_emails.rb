c8dcc6f091d569bd68907790def88335c446ca0e
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-05-25 16:29:20 +0900

    デプロイ#1

---
+++ db/migrate/20230524230137_create_emails.rb
@@ -0,0 +1,7 @@
+class CreateEmails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :emails do |t|
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_05_24_230137) do
+
+  create_table "emails", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
