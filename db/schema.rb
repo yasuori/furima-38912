7e70212f848b95d208ebc704261e1a689a9c5bce
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-05-29 12:46:30 +0900

    デプロイ＃１

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_24_230137) do
+ActiveRecord::Schema.define(version: 2023_05_29_023001) do
 
-  create_table "emails", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
