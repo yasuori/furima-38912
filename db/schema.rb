3dba180f8badffeced65c8d865ce83905d9dae67
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-05-30 07:52:46 +0900

    デプロイ/Basic認証＃１＃２

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_29_023001) do
+ActiveRecord::Schema.define(version: 2023_05_29_093615) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
