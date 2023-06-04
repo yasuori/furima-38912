e62d2667247e6bb964bb7138845649513c265ddc
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-05 07:27:37 +0900

    新規登録/本人確認情報/トップページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birthday
+  end
 end
