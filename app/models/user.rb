8552a44f0dd22c1327df99745c1591cda4a56053
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-09 20:13:53 +0900

    #4 ユーザー管理機能テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+                 
   with_options presence: true do
     validates :nickname
-
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
-      validates :last_name
-      validates :first_name
-    end
-
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-      validates :password, format: { with: VALID_PASSWORD_REGEX }
-
     validates :birthday
   end
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,10 @@ RSpec.describe User, type: :model do
       it 'passwordが6文字以上半角英数字であれば登録できる'do
         @user.password = '123abc'
         @user.password_confirmation = '123abc'
-        expect(@user).to be_valid
+        expect(@user).to be_valid 
       end
     end
+
     context '新規登録がうまくいかないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -37,7 +38,7 @@ RSpec.describe User, type: :model do
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include('Email has already been taken')
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it 'passwordが空では登録できない' do
         @user.password = ''
@@ -76,12 +77,12 @@ RSpec.describe User, type: :model do
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name can't be blank")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
       it 'last_nameが半角文字だと登録できない' do
         @user.last_name = 'yamada'
