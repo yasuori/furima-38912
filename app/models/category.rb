9bd900c1bbe029c3f596531ca704c65f03676995
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-12 15:22:33 +0900

    ＃５商品出品機能　各カテゴリープルダウン選択実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,13 +5,12 @@ class Category < ActiveHash::Base
     { id: 3, name: 'レディース' },
     { id: 4, name: 'ベビーキッズ' },
     { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' },
-    { id: 11, name: '楽器'},
-    { id: 12, name: '洋服'}
+    { id: 6, name: '本・音楽・ゲーム' },
+    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, name: '家電・スマホ・カメラ' },
+    { id: 9, name: 'スポーツ・レジャー' },
+    { id: 10, name: 'ハンドメイド' },
+    { id: 11, name: 'その他'},
   ]
 
   include ActiveHash::Associations
