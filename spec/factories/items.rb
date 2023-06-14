1ccf9da862644904c41fe92417c644591604dbfc
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-14 23:08:44 +0900

    ＃5 商品出品機能/テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+    name { "あああ" }
+    description { 'test' }
+    category_id { 2 }
+    item_status_id { 3 }
+    shipping_cost_id { 2 }
+    delivery_area_id { 2 }
+    shipping_date_id { 3 }
+    price { 3689139 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
