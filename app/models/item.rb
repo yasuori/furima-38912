093b86395d83cf1e76502a463e0a14b0516d6a17
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-17 16:36:39 +0900

    ＃５商品出品機能　コード修正0617

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
    validates :delivery_area_id
    validates :shipping_date_id
 
-   validates :price, format: { with:  /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
  
   with_options numericality: { other_than: 0 } do
