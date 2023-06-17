7806411bf8612d2c6d019ca8dd986c290da9a9f1
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-17 16:26:20 +0900

    #5 商品出品機能　コード修正0617

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,8 +21,7 @@ class Item < ApplicationRecord
    validates :delivery_area_id
    validates :shipping_date_id
 
-   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-   validates :prefecture, numericality: {other_than: 0, message: "is invalid. Input half-width characters."}
+   validates :price, format: { with:  /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
  
   with_options numericality: { other_than: 0 } do
