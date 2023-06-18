1b2d31dd3e74046be8112b436d30cf77623e93d5
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-18 15:09:12 +0900

    #6商品一覧表示機能　Rubocop修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
- extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   belongs_to :category
@@ -7,30 +7,30 @@ class Item < ApplicationRecord
   belongs_to :item_status
   belongs_to :shipping_cost
   belongs_to :shipping_date
- 
+
   has_one_attached :image
 
   validates :content, presence: true, unless: :was_attached?
- 
-  with_options presence: true do
-   validates :image
-   validates :name
-   validates :description
-   validates :category_id
-   validates :item_status_id
-   validates :shipping_cost_id
-   validates :delivery_area_id
-   validates :shipping_date_id
 
-   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :delivery_area_id
+    validates :shipping_date_id
+
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
- 
+
   with_options numericality: { other_than: 0 } do
-   validates :category_id
-   validates :delivery_area_id
-   validates :item_status_id
-   validates :shipping_cost_id
-   validates :shipping_date_id
+    validates :category_id
+    validates :delivery_area_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
   end
   validates :price, numericality: true
 end
