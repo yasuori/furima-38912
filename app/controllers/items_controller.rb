d017ebf97a8d97edb7174ce5d5dc28568cfd6745
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-19 16:14:40 +0900

    ＃7商品詳細表示機能 / Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: :show
-  
+
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
