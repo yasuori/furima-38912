e45425151119d37b0479bee9ebb681c253aa89ac
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-19 15:59:38 +0900

    #7商品詳細表示機能/商品編集・削除・購入実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: :show
+  
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -19,10 +20,30 @@ class ItemsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   item = Item.find(params[:id])
+  #   item.update(item_params)
+  # end
+
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id,
                                  :delivery_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
