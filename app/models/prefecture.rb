75641669a2a5b539a18062437418865f464cd149
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-06-25 09:05:12 +0900

    #10 発送元のコードname変更/クレジットカード決済実装

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,4 +1,4 @@
-class DeliveryArea < ActiveHash::Base
+class Prefecture < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' }, { id: 1, name: '北海道' }, { id: 2, name: '岩手県' },
     { id: 3, name: '青森県' }, { id: 4, name: '秋田県' }, { id: 5, name: '宮城県' },
