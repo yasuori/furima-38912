f42201d34ae98c89508d155b3f7ec387a98c79c5
yasuori83-10 <126753112+yasuori@users.noreply.github.com>
2023-07-09 09:11:23 +0900

    本番環境実装　修正0709

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// reqire("turbolinks").start()
+reqire("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../item_price")
-require("../card")
+require("../card.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
